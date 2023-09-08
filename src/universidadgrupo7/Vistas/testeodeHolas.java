fd8e8477f627b9e96f8b2ce35dd448a9a5acaf4e
SantiagoS0SA <141674699+SantiagoS0SA@users.noreply.github.com>
2023-09-08 16:29:57 -0300

    hola2

--- src/universidadgrupo7/Vistas/testeodeHolas.java
+++ src/universidadgrupo7/Vistas/testeodeHolas.java
@@ -3,4 +3,6 @@ package universidadgrupo7.Vistas;
 
 public class testeodeHolas {
     //hola como estas, esto se va a descontrolaaaar
+    //holiwis todo bien aca?
+    //te cuento un chiste, que hacia un chino con una capucha? capuchino!
 }
